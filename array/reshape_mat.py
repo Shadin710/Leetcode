c85352c3f28f71887630d5727192ac30b776a60a
Shadin <shadin710@gmail.com>
2022-04-18 21:31:31 +0600

    Reshape matrix

--- array/reshape_mat.py
+++ array/reshape_mat.py
@@ -2,21 +2,18 @@
 
 class Solution:
     def matrixReshape(self, mat, r: int, c: int):
-        new_arr=[]
+        if len(mat)* len(mat[0])!=r*c:
+            return mat
+        result = []
+        row = []
         for arr in mat:
             for element in arr:
-                new_arr.append(element)
-        reshaped_mat_arr = []
-        reshaped_mat = []
-        idx=0
-        if (r*c)<len(new_arr) or (r*c)>len(new_arr):
-            return mat
-        for i in range(r):
-            for j in range(c):
-                reshaped_mat_arr.append(new_arr[idx])
-                idx+=1
-            reshaped_mat.append(reshaped_mat_arr)
-        return reshaped_mat
+                row.append(element)
+                if len(row)==c:
+                    result.append(row)
+                    break
+            row=[]
+        return result
 
 if __name__ == '__main__':
     sol = Solution()
